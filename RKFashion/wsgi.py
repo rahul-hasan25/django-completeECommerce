a4e34c1428bb15e343a65fdb163331dbe7095fab
rahul-hasan25 <rahulhaasan2022@gmail.com>
2026-01-28 22:26:58 +0600

    Run migrations automatically on startup

--- RKFashion/wsgi.py
+++ RKFashion/wsgi.py
@@ -8,8 +8,18 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+import django
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RKFashion.settings')
+django.setup()
+
+# Run migrations automatically
+try:
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Migration failed: {e}")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RKFashion.settings')
 
